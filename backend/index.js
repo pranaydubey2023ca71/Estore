988d85c78cc7dbfdcc41073b208cf4497b70c26b
pranaydubey2023ca71 <156605160+pranaydubey2023ca71@users.noreply.github.com>
2025-11-13 20:34:48 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,31 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import path from 'path';
-import serverless from 'serverless-http'; // <-- Import this
-import connectDB from './db.js';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import authRoutes from './routes/authRoutes.js';
-import productRoutes from './routes/productRoutes.js';
+    import express from 'express';
+    import dotenv from 'dotenv';
+    import cors from 'cors';
+    import path from 'path';
+    import serverless from 'serverless-http'; // <-- Import this
+    import connectDB from './db.js';
+    import path, { dirname } from 'path';
+    import authRoutes from './routes/authRoutes.js';
+    import productRoutes from './routes/productRoutes.js';
 
-// --- Config ---
-dotenv.config();
-connectDB();
-const app = express();
-app.use(express.json()); 
-app.use(cors());
+    // --- Config ---
+    dotenv.config();
+    connectDB();
+    const app = express();
+    app.use(express.json()); 
+    app.use(cors());
 
-// --- API Routes ---
-app.use('/api/auth', authRoutes);
-app.use('/api/products', productRoutes);
+    // --- API Routes ---
+    app.use('/api/auth', authRoutes);
+    app.use('/api/products', productRoutes);
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-// --- Serve Static Files (Our 'uploads' folder) ---
-// This is crucial for React to display images/previews if we had them
-// For downloads, we use the protected route, but this makes the folder "web-accessible"
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+    const __dirname = dirname(__filename);
+    // --- Serve Static Files (Our 'uploads' folder) ---
+    // This is crucial for React to display images/previews if we had them
+    // For downloads, we use the protected route, but this makes the folder "web-accessible"
+    app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-// // --- Start Server ---
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-export const handler = serverless(app);
+    // // --- Start Server ---
+    // const PORT = process.env.PORT || 5000;
+    // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    export const handler = serverless(app);
